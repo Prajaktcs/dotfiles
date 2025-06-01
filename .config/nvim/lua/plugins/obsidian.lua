ba3dfecc137b8e4b589091311264ab3b84d296ae
Praj Shastry <prajakt.shastry@gmail.com>
2025-06-01 13:12:22 -0400

    Updates to config

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,52 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see above for full list of optional dependencies ☝️
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config.ClientOpts
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Documents/Projects/notes",
+      },
+    },
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "0. Landing area",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "DailyTemplate.md",
+    },
+    templates = {
+      folder = "Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+    -- see below for full list of options 👇
+  },
+
+  -- see below for full list of options 👇
+}
